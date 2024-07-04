e1ddba026c462856108fed9e6a6d42d361fcc525
Sunggggggg <sky990319@gmail.com>
2024-07-04 22:18:43 +0900

    f

---
+++ train_Motion.py
@@ -0,0 +1,49 @@
+# The code is largely borrowd from https://github.com/mkocabas/VIBE
+# Adhere to their licence to use this script
+import os
+import os.path as osp
+os.environ['PYOPENGL_PLATFORM'] = 'egl'
+
+import torch
+import pprint
+import random
+import numpy as np
+import torch.backends.cudnn as cudnn
+import importlib
+from lib.core.config import parse_args
+from lib.utils.utils import prepare_output_dir, create_logger, get_optimizer
+from lib.dataset._loaders import get_data_loaders
+from lib.utils.lr_scheduler import CosineAnnealingWarmupRestarts
+
+def main(cfg):
+    if cfg.SEED_VALUE >= 0:
+        print(f'Seed value for the experiment {cfg.SEED_VALUE}')
+        os.environ['PYTHONHASHSEED'] = str(cfg.SEED_VALUE)
+        random.seed(cfg.SEED_VALUE)
+        torch.manual_seed(cfg.SEED_VALUE)
+        np.random.seed(cfg.SEED_VALUE)
+        torch.cuda.manual_seed(cfg.SEED_VALUE)
+        torch.cuda.manual_seed_all(cfg.SEED_VALUE)
+        
+    logger = create_logger(cfg.LOGDIR, phase='train')
+
+    logger.info(f'GPU name -> {torch.cuda.get_device_name()}')
+    logger.info(f'GPU feat -> {torch.cuda.get_device_properties("cuda")}')
+
+    logger.info(pprint.pformat(cfg))
+
+    # cudnn related setting
+    cudnn.benchmark = cfg.CUDNN.BENCHMARK
+    torch.backends.cudnn.deterministic = cfg.CUDNN.DETERMINISTIC
+    torch.backends.cudnn.enabled = cfg.CUDNN.ENABLED
+
+    # ========= Dataloaders ========= #
+    data_loaders = get_data_loaders(cfg)
+
+
+
+if __name__ == '__main__':
+    cfg, cfg_file, _ = parse_args()
+    cfg = prepare_output_dir(cfg, cfg_file)
+
+    main(cfg)
