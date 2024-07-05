b46fbd0d6ca5c47fb0b4005b280c61226e754084
sky <sky990319@gmail.com>
2024-07-06 01:00:28 +0900

    f

--- lib/models/transformer.py
+++ lib/models/transformer.py
@@ -50,38 +50,6 @@ class Attention(nn.Module):
         x = self.proj_drop(x)
         return x
 
-class CrossAttention(nn.Module):
-    def __init__(self, dim, v_dim, kv_num, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
-        super().__init__()
-        self.num_heads = num_heads
-        self.kv_num = kv_num
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
-
-        self.wq = nn.Linear(dim, dim, bias=qkv_bias)
-        self.wk = nn.Linear(dim, dim, bias=qkv_bias)
-        self.wv = nn.Linear(v_dim, v_dim, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(v_dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-
-    def forward(self, xq, xk, xv):
-
-        B, N, C = xq.shape
-        v_dim = xv.shape[-1]
-        q = self.wq(xq).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)  
-        k = self.wk(xk).reshape(B, self.kv_num, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)  
-        v = self.wv(xv).reshape(B, self.kv_num, self.num_heads, v_dim // self.num_heads).permute(0, 2, 1, 3)  
-
-        attn = (q @ k.transpose(-2, -1)) * self.scale 
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-
-        x = (attn @ v).transpose(1, 2).reshape(B, N, v_dim)
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        return x
-
 class Block(nn.Module):
     def __init__(self, dim, num_heads, mlp_hidden_dim, qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
@@ -97,28 +65,6 @@ class Block(nn.Module):
         x = x + self.drop_path(self.attn(self.norm1(x)))
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
-
-class CrossAttentionBlock(nn.Module):
-    def __init__(self, q_dim, k_dim, v_dim, kv_num, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0.2, 
-                 attn_drop=0.2, drop_path=0.2, act_layer=nn.GELU, norm_layer=nn.LayerNorm):
-        super().__init__()
-        self.normq = norm_layer(q_dim)
-        self.normk = norm_layer(k_dim)
-        self.normv = norm_layer(v_dim)
-        self.kv_num = kv_num
-        self.attn = CrossAttention(q_dim, v_dim, kv_num = kv_num, num_heads=num_heads, qkv_bias=qkv_bias, 
-                                   qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
-        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(q_dim)
-        mlp_hidden_dim = int(q_dim * mlp_ratio)
-        self.mlp = Mlp(in_features=q_dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-
-    def forward(self, xq, xk, xv):
-        xq = xq + self.drop_path(self.attn(self.normq(xq), self.normk(xk), self.normv(xv)))
-        xq = xq + self.drop_path(self.mlp(self.norm2(xq)))
-
-        return xq
 
 class Transformer(nn.Module):
     def __init__(self, depth=3, embed_dim=512, mlp_hidden_dim=1024, \
