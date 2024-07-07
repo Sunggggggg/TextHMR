bb0972a37d68374d2443e3d59f55ac3267f38794
Sunggggggg <sky990319@gmail.com>
2024-07-07 16:49:23 +0900

    f

--- lib/models/text_encoder.py
+++ lib/models/text_encoder.py
@@ -16,6 +16,24 @@ class TEncoder(nn.Module):
 
         return feature
 
+class LQTEncoder(nn.Module):
+    def __init__(self, depth=3, embed_dim=512, mlp_hidden_dim=1024,
+            h=8, drop_rate=0.1, drop_path_rate=0.2, attn_drop_rate=0., length=36):
+        super().__init__()
+        num_queries = 16
+        self.input_proj = nn.Linear(768, embed_dim)
+        self.motion_query = nn.Parameter(torch.zeros((1, num_queries, embed_dim)))
+
+        
+        self.transformer = Transformer(depth=depth, length=length, embed_dim=embed_dim, mlp_hidden_dim=mlp_hidden_dim, h=h, 
+                        drop_rate=drop_rate, drop_path_rate=drop_path_rate, attn_drop_rate=attn_drop_rate)
+    
+    def forward(self, text_embed):
+        feature = self.input_proj(text_embed)
+        feature = self.transformer(feature)
+
+        return feature
+
 def get_model(depth=3, length=16, embed_dim=512, mlp_hidden_dim=1024, h=8, drop_rate=0.2, drop_path_rate=0.2, attn_drop_rate=0.):
     model = TEncoder(depth=depth, length=length, embed_dim=embed_dim, mlp_hidden_dim=mlp_hidden_dim, h=h, 
                         drop_rate=drop_rate, drop_path_rate=drop_path_rate, attn_drop_rate=attn_drop_rate)
