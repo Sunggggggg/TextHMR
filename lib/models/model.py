6aec7a52c73c0c3be558dcab3410ff992607c2b1
Sunggggggg <sky990319@gmail.com>
2024-07-07 14:13:35 +0900

    f

--- lib/models/model.py
+++ lib/models/model.py
@@ -12,17 +12,19 @@ import lib.models.linker as linker
 
 class Model(nn.Module):
     def __init__(self, 
-                 seqlen,
-                 num_joint, 
-                 embed_dim, 
-                 depth,
+                 seqlen=16,
+                 num_joints=19, 
+                 embed_dim=256, 
+                 depth=3,
                  lifter_pretrained=os.path.join(BASE_DATA_DIR, 'pose_3dpw.pth.tar')
                  ) :
         super().__init__()
-        self.pose_lifter = lifter.get_model(num_joint, embed_dim, depth, lifter_pretrained)
-        self.init_hmr = temporal_encoder.get_model(depth=depth, length=seqlen, embed_dim=embed_dim)
-        self.text_encoder = text_encoder.get_model(length=36)
-        self.linking = linker.get_model()
+        self.pose_lifter = lifter.get_model(num_joints, embed_dim, depth, lifter_pretrained)
+        self.init_hmr = temporal_encoder.get_model(depth=depth, length=seqlen, embed_dim=512)
+        self.text_encoder = text_encoder.get_model(length=36, embed_dim=512)
+        self.linking = linker.get_model(embed_dim=256)
+
+        self.proj_text = nn.Linear(512, embed_dim)
 
     def forward(self, input_text, img_feat, pose2d, is_train=False, J_regressor=None):
         """
@@ -32,11 +34,12 @@ class Model(nn.Module):
 
         pose2d = add_joint(pose2d[..., :2])
         # First stage
-        pose3d = self.pose_lifter(pose2d, img_feat)
-        init_theta = self.init_hmr(img_feat, is_train, J_regressor) 
-        text_embed = self.text_encoder(input_text)
+        pose3d = self.pose_lifter(pose2d, img_feat)                 # [B, J, 3]
+        init_theta = self.init_hmr(img_feat, is_train, J_regressor) # 
+        text_embed = self.text_encoder(input_text)                  # [B, N, ]
 
         # Second stage
+        text_embed = self.proj_text(text_embed)
         joint_guide, semantic_guide = self.linking(pose3d, text_embed)
 
         for s in init_theta:
