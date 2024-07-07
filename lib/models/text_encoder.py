a11d09a536ed7b95b7307c77cc3656643bdf0ea5
Sunggggggg <sky990319@gmail.com>
2024-07-07 14:17:26 +0900

    f

--- lib/models/text_encoder.py
+++ lib/models/text_encoder.py
@@ -6,7 +6,7 @@ class TEncoder(nn.Module):
     def __init__(self, depth=3, embed_dim=512, mlp_hidden_dim=1024,
             h=8, drop_rate=0.1, drop_path_rate=0.2, attn_drop_rate=0., length=36):
         self.input_proj = nn.Linear(768, embed_dim)
-        self.transformer = Transformer(depth=depth, seqlen=length, embed_dim=embed_dim, mlp_hidden_dim=mlp_hidden_dim, h=h, 
+        self.transformer = Transformer(depth=depth, length=length, embed_dim=embed_dim, mlp_hidden_dim=mlp_hidden_dim, h=h, 
                         drop_rate=drop_rate, drop_path_rate=drop_path_rate, attn_drop_rate=attn_drop_rate)
     
     def forward(self, text_embed):
