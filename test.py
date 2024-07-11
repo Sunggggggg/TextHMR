6deaaacead48e809c01495256d574fb420b22d8d
Sunggggggg <sky990319@gmail.com>
2024-07-11 13:32:59 +0900

    f

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import torch
-import lib.models.linker as linker
+from lib.SemHMR.model import Model
 
+img_feat = torch.rand((1, 16, 2048))
 pose3d = torch.rand((1, 19, 3))
-text_embed = torch.rand((1, 36, 256))
-model = linker.get_model()
-x, y = model(pose3d, text_embed)
-
-print(x.shape, y.shape)
+text_embed = torch.rand((1, 36, 768))
+idx = torch.tensor([[14]])
+model = Model()
+_ = model(text_embed, img_feat, pose3d, idx)
