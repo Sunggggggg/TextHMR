74a85da572a0981a835e8b339e637e39c1a1e572
Sunggggggg <sky990319@gmail.com>
2024-07-18 11:00:16 +0900

    f

--- lib/TextHMR/model_v2.py
+++ lib/TextHMR/model_v2.py
@@ -19,7 +19,7 @@ class Model(nn.Module):
         self.proj_joint = nn.Linear(3, 256)
 
         self.temp_encoder = Transformer(depth=3, embed_dim=512, mlp_hidden_dim=1024, 
-                                        h=8, drop_rate=0.1, drop_path_rate=0.2, attn_drop_rate=0., length=16)
+                                        h=8, drop_rate=0.1, drop_path_rate=0.2, attn_drop_rate=0., length=64)
         self.pre_trained_model = pre_trained_model(num_total_motion)
         self.regressor = Regressor(512+256*17)
 
