c7f9a63ad963b1be034f9edcc442cfbe3b91fd70
sky <sky990319@gmail.com>
2024-07-22 00:40:15 +0900

    f

--- script.sh
+++ script.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-python train_pretrained.py --exp_name coco_joint_64 --data_root /mnt/SKY/AMASS_proc/processed_64frames_coco_16_fps10/ --batch_size 8 --seqlen 64
-python train_v2.py --cfg configs/repr_table1_3dpw.yaml
-
-
-python train_pretrained.py --exp_name coco_joint_16_proposed --data_root /mnt/SKY/AMASS_proc/processed_16frames_coco_16_fps10/ --batch_size 8 --seqlen 16
-#python train_pretrained.py --exp_name coco_joint_64_KIT --data_root /mnt/SKY/AMASS_proc/processed_64frames_coco_16_fps10_KIT/ --batch_size 8 --seqlen 64
+python train_v2.py --cfg configs/repr_table1_3dpw_v3.yaml
+python train_v3.py --cfg configs/repr_table1_3dpw.yaml
