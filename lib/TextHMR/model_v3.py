b2051a52aa82792209173e161e94b1249cc5037f
sky <sky990319@gmail.com>
2024-07-23 01:20:39 +0900

    f

--- lib/TextHMR/model_v3.py
+++ lib/TextHMR/model_v3.py
@@ -18,9 +18,8 @@ class Model(nn.Module):
         self.seqlen = seqlen
         self.text_archive = text_archive
 
-        self.proj_img1 = nn.Linear(2048, 512)
-        self.proj_img2 = nn.Linear(512, 256)
-        self.temp_encoder = Transformer(depth=3, embed_dim=512, mlp_hidden_dim=2048, 
+        self.proj_img = nn.Linear(2048, 256)
+        self.temp_encoder = Transformer(depth=3, embed_dim=256, mlp_hidden_dim=1024, 
                                         h=8, drop_rate=0.1, drop_path_rate=0.2, attn_drop_rate=0., length=64)
         self.pre_trained_model = pre_trained_model(num_total_motion, seqlen)
         self.regressor = SMPLRegressor(dim_rep=256)
@@ -45,9 +44,8 @@ class Model(nn.Module):
 
         joint_feat = self.pre_trained_model.extraction_features(pose_2d, self.text_archive)   # [B, T, J, 256]
         
-        img_feat = self.proj_img1(f_img)                     
+        img_feat = self.proj_img(f_img)                     
         img_feat = self.temp_encoder(img_feat)              # [B, T, 512]
-        img_feat = self.proj_img2(img_feat)                 # [B, T, 256]
 
         if is_train:
             global_joint_feat = joint_feat
